7191e7cf94d0370283cffa4e6756c2045bce2eb4
Kseniia Skaletska <57195906+Kseniask@users.noreply.github.com>
2020-05-30 18:31:05 -0700

    Update kcreads.js

--- kcreads.js
+++ kcreads.js
@@ -1,50 +1,53 @@
-window.onscroll = function(){fixedheader();}
-var header = document.getElementById("myheader");
-var sticky = header.offsetTop;
+window.onscroll = function () {
+  fixedheader()
+}
+var header = document.getElementById('myheader')
+var sticky = header.offsetTop
 
-function fixedheader(){
-    if(window.pageYOffset > sticky){
-        header.classList.add("sticky");
-    }
-    else{
-        header.classList.remove("sticky");
-    }
+function fixedheader () {
+  if (window.pageYOffset > sticky && screen.width < 640) {
+    header.classList.add('sticky')
+  } else {
+    header.classList.remove('sticky')
+  }
 }
 
+function Readmore () {
+  var dots = document.getElementById('dots')
+  var moretext = document.getElementById('more')
+  var btnText = document.getElementById('myBtn')
 
-function Readmore(){
-    var dots = document.getElementById("dots");
-    var moretext = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-
-    if(dots.style.display ==="none"){
-        dots.style.display = "inline";
-        moretext.style.display = "none";
-        btnText.innerHtml = "Read more";
-    }
-    else{
-        dots.style.display = "none";
-        moretext.style.display = "inline";
-        btnText.innerHTML = "Read less";
-    }
-    
+  if (dots.style.display === 'none') {
+    dots.style.display = 'inline'
+    moretext.style.display = 'none'
+    btnText.innerHtml = 'Read more'
+  } else {
+    dots.style.display = 'none'
+    moretext.style.display = 'inline'
+    btnText.innerHTML = 'Read less'
+  }
 }
 
-function Readmore1(){
-    var dots = document.getElementById("dots1");
-    var moretext = document.getElementById("more1");
-    var btnText = document.getElementById("myBtn1");
+function Readmore1 () {
+  var dots = document.getElementById('dots1')
+  var moretext = document.getElementById('more1')
+  var btnText = document.getElementById('myBtn1')
 
-    if(dots.style.display ==="none"){
-        dots.style.display = "inline";
-        moretext.style.display = "none";
-        btnText.innerHtml.Text = "Read more";
-    }
-    else{
-        dots.style.display = "none";
-        moretext.style.display = "inline";
-        btnText.innerHTML = "Read less";
-    }
-    
+  if (dots.style.display === 'none') {
+    dots.style.display = 'inline'
+    moretext.style.display = 'none'
+    btnText.innerHtml.Text = 'Read more'
+  } else {
+    dots.style.display = 'none'
+    moretext.style.display = 'inline'
+    btnText.innerHTML = 'Read less'
+  }
 }
-
+var menu = document.getElementById('links')
+function burgerMenu () {
+  if (menu.id == 'links') {
+    menu.id = 'burger'
+  } else {
+    menu.id = 'links'
+  }
+}
